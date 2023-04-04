fe6202193616efffe7774e27ae610a320bccace2
artemov <akiberq@gmail.com>
2023-04-04 03:34:58 +0200

    Add binary search index

--- lib/src/algos/base/binary_search.dart
+++ lib/src/algos/base/binary_search.dart
@@ -1,1 +1,32 @@
+class BinarySearch {
+  final List<num> numbers;
 
+  BinarySearch(this.numbers);
+
+  int indexOf(num searchedNumber) {
+    var left = 0;
+    var right = numbers.length;
+
+    while (left < right) {
+      var mid = (right - left) ~/ 2;
+      var currentIndex = left + mid;
+      var checkedNumber = numbers[currentIndex];
+
+      if (checkedNumber == searchedNumber) {
+        return currentIndex;
+      } else {
+        if (checkedNumber > searchedNumber) {
+          right = mid;
+        } else {
+          left = mid;
+        }
+      }
+    }
+
+    return -1;
+  }
+}
+
+extension BinarySearchX on List<num> {
+  int binarySearchOf(num number) => BinarySearch(this).indexOf(number);
+}
--- test/algo_test.dart
+++ test/algo_test.dart
@@ -1,6 +1,12 @@
-import 'package:algo/algo.dart';
+import 'package:algo/src/algos/base/binary_search.dart';
 import 'package:test/test.dart';
 
 void main() {
-
+  test('binary_search', () {
+    expect(
+        List.of(
+          [-101, -5, 10, 10, 12, 128],
+        ).binarySearchOf(12),
+        4);
+  });
 }
